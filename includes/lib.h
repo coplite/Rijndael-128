d9cf50d2cff91fc5d15e4a8c000dd3694c9ae260
coplite <96002960+coplite@users.noreply.github.com>
2025-04-04 20:03:48 -0700

    Update lib.h

--- includes/lib.h
+++ includes/lib.h
@@ -6,18 +6,19 @@
 #define BLOCK_SIZE 16
 #define xtime(x) (uint8_t)(((x) << 1) ^ ((((x) >> 7) & 1) * 0x1B)) 
 
-struct key_wrapper {
+typedef struct {
     uint8_t key[BLOCK_SIZE];
-    uint8_t round_key[176];  // 16 * 11 = 176 bytes
-};
+    uint8_t round_key[176];  // BLOCK_SIZE * 11 = 176 bytes
+}key_wrapper;
 
-void encrypt(uint8_t* data, struct key_wrapper* keys);
-void decrypt(uint8_t* data, struct key_wrapper* keys);
+void encrypt(uint8_t* data, key_wrapper* keys);
+void decrypt(uint8_t* data, key_wrapper* keys);
+
 void key_expansion(uint8_t* key, uint8_t* round_key);
-void bytes_matrix(uint8_t* input, uint8_t matrix[4][4]);
-void matrix_bytes(uint8_t* output, uint8_t matrix[4][4]);
+
 void pcks7(uint8_t* input);
 void inv_pcks7(uint8_t* input);
-size_t _strlen(char* input);
+
 void _memcpy(void *to, const void *from, size_t numBytes);
 void _memset(void* ptr, int value, size_t num);
+size_t _strlen(char* input);
